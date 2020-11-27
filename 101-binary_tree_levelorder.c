3be49a5d1842985bfa3465ab28fb5b22e2c43fed
santiagopinzonD <santiagopinzon456@gmail.com>
2020-11-27 16:25:24 +0000

    fix betty

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_levelorder - function that goes through a binary tree using
+* using level-order traversal
+* @tree: the tree
+* @func: pointer to function that print
 *
-*
-*
-*
-*
-*
+* Return: void
 */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
@@ -53,6 +53,15 @@ int max(int a, int b)
 	return ((a >= b) ? a : b);
 }
 
+/**
+ * func_level - function that print each level
+ *
+ * @tree: the tree
+ * @level: each level of the tree
+ * @func: pointer to function that print
+ *
+ * Return: Void
+ */
 void func_level(const binary_tree_t *tree, int level, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
